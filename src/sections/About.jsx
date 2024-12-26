abfbf5df239797797bcf36c5af51123631013a0b
MuxN4 <stev3npy@gmail.com>
2024-12-26 11:44:10 +0300

    About section component

---
+++ src/sections/About.jsx
@@ -0,0 +1,95 @@
+import React from 'react'
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import { skills, experiences } from '../constants'
+
+const About = () => {
+  return (
+    <section className='max-container'>
+      <h1 className='head-text'>
+        Hello, I'm <span className='blue-gradient_text font-semibold drop-shadow'>Eve Nicole</span>
+      </h1>
+      <div className='mt-5 flex flex-col gap-3 text-slate-600'>
+        <p>
+          I'm a skilled software developer with experience in Python and
+          JavaScript, and expertise in frameworks like React, Node.js, and
+          Django. I'm a quick learner and collaborate closely with clients to
+          create efficient, scalable, and user-friendly solutions that solve
+          real-world problems. Let's work together to bring your ideas to life!        
+        </p>
+      </div>
+      <div className='py-10 flex flex-col'>
+        <h3 className='subhead-text'>My SKills</h3>
+
+        <div className='mt-16 flex flex-wrap gap-12'>
+          {skills.map((skill) => (
+            <div className='block-container w-20 h-20'>
+              <div className='btn-back rounded-xl'/>
+              <div className='btn-front rounded-xl justify-center items-center'>
+                <img 
+                  src={skill.imageUrl}
+                  alt={skill.name}
+                  className='w-1/2 h-1/2 object-contain'
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+
+      </div>
+
+      <div>
+        <h3 className='subhead-text'>Work Experience</h3>
+        <div className='mt-5 flex flex-col gap-3 text-slate-600'>
+          <p>
+            Throughout my career, I've collaborated with diverse companies, enhancing my skills and working alongside talented individuals. Here's a quick overview: 
+          </p>
+        </div>
+        <div className='mt-12 flex'>
+          <VerticalTimeline>
+            {experiences.map((experience) => (
+              <VerticalTimelineElement 
+                key={experience.company_name} 
+                date={experience.date}
+                icon={
+                  <div className='flex justify-center items-center w-full h-full'> 
+                    <img 
+                      src={experience.icon}
+                      alt={experience.company_name}
+                      className='w-[60%] h-[60%] object-contain'
+                    />
+                  </div>
+                }
+                iconStyle={{background: experience.iconBg}}
+                contentStyle={{
+                  borderBottom: '8px',
+                  borderStyle: 'solid',
+                  borderBottomColor: experience.iconBg,
+                  boxShadow: 'none'
+                }}
+              >
+                <div>
+                  <h3 className='text-black text-xl font-poppins font-semibold'>
+                    {experience.title}
+                  </h3>
+                  <p className='text-black-500 font-medium font-base' style={{margin:0}}>
+                    {experience.company_name}
+                  </p>
+                </div>
+                <ul className='my-5 list-disc ml-5 space-y-2'>
+                  {experience.points.map((point, index) => (
+                    <li key={`experience-point-${index}`} className='text-black-500/50 font-normal pl-1 text-sm'>
+                      {point}
+                    </li>
+                  ))}
+                </ul>
+              </VerticalTimelineElement>
+            ))}
+          </VerticalTimeline>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default About
