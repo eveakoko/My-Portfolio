43397638b47b1ab35b4028c91599c5e89c95d4b2
MuxN4 <stev3npy@gmail.com>
2025-01-02 16:11:31 +0300

    updated assets

--- public/assets/index.js
+++ public/assets/index.js
@@ -1,36 +1,39 @@
-import css from './css.svg'
-import express from './express.svg'
-import git from './git.svg'
-import github from './github.svg'
-import html from './html.svg'
-import javascript from './javascript.svg'
-import mongodb from './mongodb.svg'
-import nextjs from './nextjs.svg'
-import nodejs from './nodejs.svg'
-import react from './react.svg'
-import sqlalchemy from './sqlalchemy.svg'
-import python from './python.svg'
-import django from './django.svg'
-import postman from './postman.svg'
-import flask from './flask.svg'
-import tailwindcss from './tailwindcss.svg'
+// import css from './css.svg'
+// import express from './express.svg'
+// import git from './git.svg'
+// import github from './github.svg'
+// import html from './html.svg'
+// import javascript from './javascript.svg'
+// import mongodb from './mongodb.svg'
+// import nextjs from './nextjs.svg'
+// import nodejs from './nodejs.svg'
+// import react from './react.svg'
+// import sqlalchemy from './sqlalchemy.svg'
+// import python from './python.svg'
+// import django from './django.svg'
+// import postman from './postman.svg'
+// import flask from './flask.svg'
+// import tailwindcss from './tailwindcss.png'
+// import vite from '.vite.svg'
+// import mySQL from '.mySQL.svg'
 
-export {
-    postman,
-    react,
-    flask,
-    sqlalchemy,
-    nextjs,
-    html,
-    css,
-    tailwindcss,
-    github,
-    mySQL,
-    express,
-    git,
-    django,
-    mongodb,
-    python,
-    javascript,
-    nodejs
-  } 
+// export {
+//     postman,
+//     react,
+//     flask,
+//     sqlalchemy,
+//     nextjs,
+//     html,
+//     css,
+//     tailwindcss,
+//     github,
+//     mySQL,
+//     express,
+//     git,
+//     django,
+//     mongodb,
+//     python,
+//     javascript,
+//     nodejs,
+//     vite
+//   } 
